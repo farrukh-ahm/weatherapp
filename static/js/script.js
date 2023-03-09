74c62facf99c5f6f57906f73f4c78dc5001f1354
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2023-03-09 10:51:47 +0000

    Add function to show the location, time, current temp, condition, feels like and wind

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,18 @@
+let loc = document.getElementById("location")
+let time = document.getElementById("time")
+let temp = document.getElementById("temp")
+let condition = document.getElementById("condition")
+let feels = document.getElementById("feels")
+let wind = document.getElementById("wind")
+let max = document.getElementById("max")
+let min = document.getElementById("min")
+
+
 document.addEventListener("DOMContentLoaded", ()=>{
 
-    initiatePosition()
+    initiatePosition();
+
+    setInterval(getTime, 100);
 
 })
 
@@ -22,11 +34,50 @@ const getPosition = (location) =>{
     const long = location.coords.longitude;
 
     console.log(lat, long)
-    return (lat, long)
+    getWeather(lat, long)
 }
 
 
+const getWeather = async (lat, long) =>{
 
+    const response = await fetch (`https://api.weatherapi.com/v1/current.json?key=${keys.KEY}&q=${lat},${long}&aqi=no`)
+
+    const data = await response.json()
+
+    if(response.ok){
+
+        displayWeather(data)
+        console.log(data)
+    }
+
+    else{
+        console.log(error)
+    }
+
+}
+
+
+const displayWeather = (data) =>{
+
+    loc.innerText = `${data.location.name}, ${data.location.country}`
+
+    temp.innerHTML = `${data.current.temp_c}<sup>o</sup>C`
+
+    condition.innerText = `${data.current.condition.text}`
+
+    feels.innerHTML = `Feels Like: ${data.current.feelslike_c}<sup>o</sup>C`
+
+    wind.innerText = `Wind: ${data.current.wind_kph}kmph`
+
+    // max.innerHTML = `${data.}`
+
+}
+
+
+const getTime = () =>{
+    let currentTime = new Date()
+    time.innerText = currentTime.toLocaleTimeString();
+}
 
 const errorCode = (error) => {
     console.log(error)
